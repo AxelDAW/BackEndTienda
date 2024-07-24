203a742f224c1c0c8d3f0fbffe7786905fc3f43a
AxelDAW <axelfreire18@gmail.com>
2024-07-24 13:52:31 +0200

    newRoutes

--- routes/Products.js
+++ routes/Products.js
@@ -23,4 +23,51 @@ module.exports = function ( app ){
 
     })
 
+    app.post('/perfil/subirprod', (req, res) => {
+
+        let {nombre, desc, precio, dispo, categoria,imagen} = req.body;
+
+        db.query(`Insert into productos (nombre, descripcion, precio, disponibilidad, categoria, imagen)
+            values ('${nombre}', '${desc}', ${precio}, ${dispo}, ${categoria}, '${imagen}')`, (err, results) => {
+
+                if (err) {
+
+                    console.error("Error en la consulta", err);
+                    res.json({msg: 'Error del servidor'});
+
+                } else {
+
+                    res.json(results)
+
+                }
+
+            })
+
+    })
+
+    app.get('/menu/producto/:id', (req, res) =>{
+
+        db.query('Select * from productos where id = ' + req.params.id, (err, results) => {
+
+            if (err) {
+
+                console.error('Error en la consulta', err);
+                res,json({ msg: "Error en el servidor."});
+
+            } else {
+
+                res.json(results);
+
+            }
+
+        })
+
+    })
+
+    app.get('/categorias', (req, res) => {
+
+        db.query()
+
+    })
+
 }
